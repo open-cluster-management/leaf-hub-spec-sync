2958b440efd6a9714846d151123c4cc1c101e243
Maroon Ayoub <maroon.ayoub@ibm.com>
2022-03-26 00:17:53 +0300

    fix case where namespace is empty
    Signed-off-by: Maroon Ayoub <maroon.ayoub@ibm.com>

--- pkg/controller/helpers/helpers.go
+++ pkg/controller/helpers/helpers.go
@@ -52,6 +52,10 @@ func DeleteObject(ctx context.Context, k8sClient client.Client, obj *unstructure
 
 // CreateNamespaceIfNotExist creates a namespace in case it doesn't exist.
 func CreateNamespaceIfNotExist(ctx context.Context, k8sClient client.Client, namespace string) error {
+	if namespace == "" {
+		return nil // objects with no namespace such as ManagedClusterSet
+	}
+
 	namespaceObj := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{Name: namespace},
 	}
