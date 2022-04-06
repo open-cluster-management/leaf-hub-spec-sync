f5365134f34d701511c15faa5f4d0ed309f6eddc
Maroon Ayoub <maroon.ayoub@ibm.com>
2022-04-06 23:21:22 +0300

    minor bugfix
    Signed-off-by: Maroon Ayoub <maroon.ayoub@ibm.com>

--- pkg/transport/kafka/consumer.go
+++ pkg/transport/kafka/consumer.go
@@ -215,7 +215,7 @@ func (c *Consumer) processMessage(msg *kafka.Message) {
 
 func (c *Consumer) syncGenericBundle(payload []byte) error {
 	receivedBundle := bundle.NewGenericBundle()
-	if err := json.Unmarshal(payload, &receivedBundle); err != nil {
+	if err := json.Unmarshal(payload, receivedBundle); err != nil {
 		return fmt.Errorf("failed to parse bundle - %w", err)
 	}
 
--- pkg/transport/sync-service/sync_service.go
+++ pkg/transport/sync-service/sync_service.go
@@ -186,7 +186,7 @@ func (s *SyncService) handleBundles(ctx context.Context) {
 
 func (s *SyncService) syncGenericBundle(payload []byte) error {
 	receivedBundle := bundle.NewGenericBundle()
-	if err := json.Unmarshal(payload, &receivedBundle); err != nil {
+	if err := json.Unmarshal(payload, receivedBundle); err != nil {
 		return fmt.Errorf("failed to parse bundle - %w", err)
 	}
 
